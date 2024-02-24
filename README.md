ca6f38f23d7cd671decde4ce2f17a29a8ee41295
SA6MWA Michel <sa6mwa@gmail.com>
2024-02-25 00:15:53 +0100

    Add fully functional version (#1)
    * Save
    
    * Save
    
    * Save

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # containerd-deployable
-containerd with rootlesskit, buildkitd and associated CLIs packaged into static binaries
+
+The `build.sh` script in this repository builds all components necessary to run containerd in a rootless environment. The script is intended to be run on a system with a working Go installation. The script will download and build the following components:
+
+* runc
+* containerd
+* nerdctl
+* plugins <https://github.com/containernetworking/plugins>
+* buildkit
+* rootlesskit
+* slirp4netns
+
+All components will be statically linked and compressed into a tarball
+for easy distribution. The tarball can be extracted under the root
+directory of the target system and the binaries will end up in
+`/usr/local/bin` and `/usr/bin`. CNI plugins will be installed in
+`/opt/cni/bin`.
+
+```console
+$ ./build.sh
+```
+
+The script will create a tarball in the `build` directory. See the
+release page for pre-built tarballs.
+
+## Usage
+
+Extract the tarball on the target system from the root directory.
+
+```console
+$ sudo tar -C / -xvzf containerd-deployable-0.1.tar.gz
+```
+
+Become a normal user you want to run containerd as and run...
+
+```console
+$ contrainerd-rootless-setuptool.sh check
+[INFO] Checking RootlessKit functionality
+[INFO] Checking cgroup v2
+[INFO] Checking overlayfs
+[INFO] Requirements are satisfied
+```
+
+The helper script `setup-rootless-containerd.sh` (located under
+`usr/local/bin`) can be used to set up potentially missing systemd
+units for delegating cpu, cpuset, etc to the user namespace. On most
+modern systems, this is not necessary. If
+`containerd-rootless-setuptool.sh` says everything is fine, the script
+will run `containerd-rootless-setuptool.sh install` and enable
+lingering mode for the user (`sudo loginctl enable-linger $(whoami)`).
+
+When complete, you should be able to run a container image from
+`docker.io`...
+
+```console
+$ nerdctl run --rm -ti busybox
+docker.io/library/busybox:latest:                                                 resolved       |++++++++++++++++++++++++++++++++++++++| 
+index-sha256:6d9ac9237a84afe1516540f40a0fafdc86859b2141954b4d643af7066d598b74:    done           |++++++++++++++++++++++++++++++++++++++| 
+manifest-sha256:538721340ded10875f4710cad688c70e5d0ecb4dcd5e7d0c161f301f36f79414: done           |++++++++++++++++++++++++++++++++++++++| 
+config-sha256:3f57d9401f8d42f986df300f0c69192fc41da28ccc8d797829467780db3dd741:   done           |++++++++++++++++++++++++++++++++++++++| 
+layer-sha256:9ad63333ebc97e32b987ae66aa3cff81300e4c2e6d2f2395cef8a3ae18b249fe:    done           |++++++++++++++++++++++++++++++++++++++| 
+elapsed: 2.8 s                                                                    total:  2.1 Mi (777.9 KiB/s)                                     
+/ # echo hello world
+hello world
+/ # 
+```
---
+++ build.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+set -eo pipefail
+
+VERSION="0.1"
+
+export CGO_ENABLED=0
+export GOFLAGS=-trimpath
+
+mkdir -p build
+pushd build
+
+sudo apt-get install build-essential autoconf libglib2.0-dev libcap-dev libseccomp-dev meson 
+
+OUT=$(pwd)/out
+PREFIX=$OUT/usr/local
+CNI_BIN=$OUT/opt/cni/bin
+mkdir -p $PREFIX $CNI_BIN
+
+repos=(
+	"opencontainers/runc"
+	"containerd/containerd"
+	"containerd/nerdctl"
+	"containernetworking/plugins"
+	"moby/buildkit"
+	"rootless-containers/rootlesskit"
+	"rootless-containers/slirp4netns"
+	"slirp/libslirp"
+)
+
+for repo in "${repos[@]}"; do
+		if [ -d "$(basename ${repo})" ]; then
+			echo "skipping ${repo}"
+			continue
+		fi
+		if [ "${repo}" = "slirp/libslirp" ]; then
+				git clone --depth=1 https://gitlab.freedesktop.org/${repo}.git
+		else				
+				git clone --depth=1 https://github.com/${repo}.git
+		fi
+done
+
+# build libslirp
+pushd libslirp
+meson build --default-library=static
+ninja -C build
+LIBSLIRP_A=$(realpath build)
+popd
+
+# build runc
+pushd runc
+make static
+make install PREFIX=$PREFIX
+popd
+
+# build containerd
+pushd containerd
+make
+make install PREFIX=$PREFIX
+popd
+
+# build plugins
+pushd plugins
+GOFLAGS=-trimpath ./build_linux.sh
+install -m 0755 -t $CNI_BIN bin/*
+popd
+
+# build slirp4netns
+pushd slirp4netns
+./autogen.sh
+./configure --prefix=$OUT/usr LDFLAGS="-static -L${LIBSLIRP_A}"
+make
+make install
+popd
+
+# build rootlesskit
+pushd rootlesskit
+make
+make install DESTDIR=$OUT
+popd
+
+# build buildkit
+pushd buildkit
+go build -gcflags 'all=-N -l' -ldflags "-extldflags '-static'" -tags "osusergo netgo static_build seccomp" ./cmd/buildkitd
+go build -gcflags 'all=-N -l' -ldflags "-extldflags '-static'" ./cmd/buildctl
+install -m 0755 -t $PREFIX/bin buildkitd buildctl
+popd
+
+# build nerdctl
+pushd nerdctl
+make
+make install PREFIX=$PREFIX
+install -m 0755 -t $PREFIX/bin extras/rootless/*
+popd
+
+mkdir -p $OUT/usr/local/share/containerd
+cat <<EOF > $OUT/usr/local/share/containerd/systemd-user.service
+[Unit]
+Description=User Manager for UID %i
+After=systemd-user-sessions.service
+# These are present in the RHEL8 version of this file except that the unit is Requires, not Wants.
+# It's listed as Wants here so that if this file is used in a RHEL7 settings, it will not fail.
+# If a user upgrades from RHEL7 to RHEL8, this unit file will continue to work until it's
+# deleted the next time they upgrade Tableau Server itself.
+After=user-runtime-dir@%i.service
+Wants=user-runtime-dir@%i.service
+
+[Service]
+LimitNOFILE=infinity
+LimitNPROC=infinity
+User=%i
+PAMName=systemd-user
+Type=notify
+# PermissionsStartOnly is deprecated and will be removed in future versions of systemd
+# This is required for all systemd versions prior to version 231
+PermissionsStartOnly=true
+ExecStartPre=/bin/loginctl enable-linger %i
+ExecStart=-/lib/systemd/systemd --user
+Slice=user-%i.slice
+KillMode=mixed
+Delegate=yes
+TasksMax=infinity
+Restart=always
+RestartSec=15
+
+[Install]
+WantedBy=default.target
+EOF
+
+cat <<EOF > $OUT/usr/local/bin/setup-rootless-containerd.sh
+#!/bin/sh
+set -uo pipefail
+echo "INFO: Running containerd-rootless-setuptool.sh check"
+
+OUTPUT=\$(containerd-rootless-setuptool.sh check 2>&1)
+
+set -e
+
+if echo "\$OUTPUT" | grep -q -e ERROR -e WARNING; then
+	if echo "\$OUTPUT" | grep -q "Needs systemd"; then
+	 		echo "INFO: Copying /usr/local/share/containerd/systemd-user.service to /etc/systemd/system/user@\$(id -u).service"	
+	 		sudo cp /usr/local/share/containerd/systemd-user.service /etc/systemd/system/ /etc/systemd/system/user@\$(id -u).service
+	 		echo "INFO: Running sudo systemctl daemon-reload"
+	 		sudo systemctl daemon-reload
+			echo "INFO: Running sudo systemctl enable user@\$(id -u).service"
+			sudo systemctl enable user@\$(id -u).service
+			echo "INFO: Running sudo systemctl start user@\$(id -u).service"
+			sudo systemctl start user@\$(id -u).service
+	fi
+
+  if [ ! -e /sys/fs/cgroup/cgroup.controllers ]; then
+	  echo "ERROR: /sys/fs/cgroup/cgroup.controllers does not exist"
+	  exit 1
+  fi
+  if ! grep cpu /sys/fs/cgroup/cgroup.controllers | grep memory | grep cpuset | grep -q io; then
+	  echo "ERROR: some cgroup controllers are not enabled"
+	  echo "INFO: Enabling the missing cgroup controllers"
+    sudo mkdir -p /etc/systemd/system/user@.service.d
+		cat <<EOF2 | sudo tee /etc/systemd/system/user@.service.d/delegate.conf
+[Service]
+Delegate=cpu cpuset io memory pids
+EOF2
+    echo "INFO: Running sudo systemctl daemon-reload"
+		sudo systemctl daemon-reload
+  fi
+	if ! containerd-rootless-setuptool.sh check ; then
+		echo "ERROR: containerd-rootless-setuptool.sh check failed"
+		exit 1
+	fi
+fi
+
+echo "INFO: Running containerd-rootless-setuptool.sh install"
+containerd-rootless-setuptool.sh install
+systemctl --user status containerd.service
+
+echo "INFO: Enabling lingering to keep containerd running after logout"
+echo "INFO: Running sudo loginctl enable-linger \$(whoami)"
+sudo loginctl enable-linger \$(whoami)
+
+echo "INFO: To install buildkit, run the following command: containerd-rootless-setuptool.sh install-buildkit"
+EOF
+chmod 0755 $OUT/usr/local/bin/setup-rootless-containerd.sh
+
+
+tar -cvzf containerd-deployable-${VERSION}.tar.gz --owner=root --group=root --transform 's/^out\///g' out/*
+
+popd
